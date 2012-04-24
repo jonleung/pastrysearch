7746ddaec156302bfe963fb877cf8c51f571e4b2
Jonathan <naruto137@gmail.com>
2012-04-24 16:25:03 -0400

    blank new rails project"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PastrySearch::Application.config.secret_token = 'feca39951dc5f95ee37b8b53708cf98b13aa67763c2c254ada43e9eb7e21f8ec4c1bf85317eb28c24625ec382df4c58e9ce3582586cb877c2dddaef137399ec7'
